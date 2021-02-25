5b3a34b55533ac970df1b65786f7975b0cd69bd4
sifat007 <sifat27.ht@gmail.com>
2021-02-25 18:31:02 +0600

    signin done

--- routes/auth.js
+++ routes/auth.js
@@ -8,6 +8,7 @@ router.get('/', (req, res) => {
     res.send("hello");
 });
 
+//signup route
 router.post('/signup', (req, res) => {
     const {name,email,password} = req.body 
   if(!email || !password || !name){
@@ -44,5 +45,32 @@ router.post('/signup', (req, res) => {
 
   })
 });
+
+//sigin route
+router.post('/signin',(req,res)=>{
+  const {email,password} = req.body
+  if(!email || !password){
+     return res.status(422).json({error:"please add email or password"})
+  }
+  User.findOne({email:email})
+  .then(savedUser=>{
+      if(!savedUser){
+         return res.status(422).json({error:"Invalid Email or password"})
+      }
+      bcrypt.compare(password,savedUser.password)
+      .then(doMatch=>{
+          if(doMatch){
+              res.json({message:"successfully signed in"})
+          }
+          else{
+              return res.status(422).json({error:"Invalid Email or password"})
+          }
+      })
+      .catch(err=>{
+          console.log(err)
+      })
+  })
+})
+
 
 module.exports=router;
