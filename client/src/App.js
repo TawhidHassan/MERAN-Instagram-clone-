3d7e426ca454590cfb2aca91c4d91f50bc7e5fda
sifat hassan <sifat007.sh7@gmail.com>
2021-04-01 00:51:48 +0600

    create post ui

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/screens/Home'
 import Profile from './components/screens/Profile'
 import Signup from './components/screens/Signup'
 import Login from './components/screens/Login'
+import CretePost from './components/screens/CreatePost'
 function App() {
   return (
     <BrowserRouter>
@@ -21,6 +22,9 @@ function App() {
       </Route>
       <Route path="/profile">
         <Profile></Profile>
+      </Route>
+      <Route path="/create">
+        <CretePost></CretePost>
       </Route>
 
     </BrowserRouter>
---
+++ client/src/components/screens/CreatePost.js
@@ -0,0 +1,46 @@
+import React,{useState,useEffect} from 'react'
+
+import {useHistory} from 'react-router-dom'
+
+const CretePost = ()=>{
+
+    return(
+        <div className="card input-filed"
+        style={{
+            margin:"30px auto",
+            maxWidth:"500px",
+            padding:"20px",
+            textAlign:"center"
+        }}
+        >
+            <input 
+            type="text"
+             placeholder="title"
+             
+             />
+            <input
+             type="text"
+              placeholder="body"
+              
+              />
+            <div className="file-field input-field">
+             <div className="btn #64b5f6 blue darken-1">
+                 <span>Uplaod Image</span>
+                 <input type="file"  />
+             </div>
+             <div className="file-path-wrapper">
+                 <input className="file-path validate" type="text" />
+             </div>
+             </div>
+             <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+             
+             
+             >
+                 Submit post
+             </button>
+ 
+        </div>
+    )
+}
+
+export default CretePost
