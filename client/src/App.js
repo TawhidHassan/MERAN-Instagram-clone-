54eb2b7a81dbde3e5e2c5a6ef1e776b8db3d0095
sifat hassan <sifat007.sh7@gmail.com>
2021-04-01 00:34:06 +0600

    navbar make and link build also

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Profiler } from 'react'
+import NavBAr from './components/NavBar'
+import "./App.css"
+import {BrowserRouter,Route,Switch,useHistory} from 'react-router-dom'
+import Home from './components/screens/Home'
+import Profile from './components/screens/Profile'
+import Signup from './components/screens/Signup'
+import Login from './components/screens/Login'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBAr></NavBAr>
+      <Route exact path="/">
+        <Home></Home>
+      </Route>
+      <Route path="/login">
+        <Login></Login>
+      </Route>
+      <Route path="/signup">
+        <Signup></Signup>
+      </Route>
+      <Route path="/profile">
+        <Profile></Profile>
+      </Route>
+
+    </BrowserRouter>
+   
   );
 }
 
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Link ,useHistory} from 'react-router-dom'
+const NavBar=()=>{
+    return(
+        <nav>
+        <div className="nav-wrapper white">
+          <a className="brand-logo"><Link to="/">Instagram</Link></a>
+          <ul id="nav-mobile" className="right">
+            <li key="1"><i className="large material-icons">search</i></li>,
+            <li key="2"><Link to="/profile">Profile</Link></li>,
+            <li key="3"><Link to="/create">Create Post</Link></li>,
+            <li key="4"><Link to="/myfollowingpost">My following Posts</Link></li>,
+            <li key="2"><Link to="/login">login</Link></li>,
+            <li key="2"><Link to="/signup">SignUp</Link></li>,
+          </ul>
+        </div>
+      </nav>
+    );
+}
+
+
+export default NavBar;
