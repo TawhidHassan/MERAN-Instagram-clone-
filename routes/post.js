fab98f2135a4154ef377371573bd67b9e11eaf84
sifat hassan <sifat007.sh7@gmail.com>
2021-04-03 10:23:42 +0600

    See posts of my followings api

--- routes/post.js
+++ routes/post.js
@@ -35,6 +35,21 @@ router.get('/allpost',requireLogin,(req,res)=>{
     
 })
 
+router.get('/getsubpost',requireLogin,(req,res)=>{
+
+    // if postedBy in following
+    Post.find({postedBy:{$in:req.user.following}})
+    .populate("postedBy","_id name")
+    .populate("comments.postedBy","_id name")
+    .sort('-createdAt')
+    .then(posts=>{
+        res.json({posts})
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+})
+
 
 router.get('/mypost',requireLogin,(req,res)=>{
     Post.find({postedBy:req.user._id})
