577c015fd49691de97c7abcba7884cc8d25b1d9e
sifat hassan <sifat007.sh7@gmail.com>
2021-04-03 08:30:16 +0600

    API to see the profile of other users

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -31,7 +31,7 @@ const Home = () => {
             })
         }).then(res => res.json())
             .then(result => {
-                //   console.log(result)
+                  console.log(result)
                 const newData = data.map(item => {
                     if (item._id == result._id) {
                         return result
