f0cbd2deda88ec45341c7186def79a7e3c8c50ad
sifat hassan <sifat007.sh7@gmail.com>
2021-04-03 09:09:40 +0600

    API for follow and unfollow

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema.Types
 const Schema = mongoose.Schema
 
 //Create Schema
@@ -19,7 +20,13 @@ const userSchema = new Schema({
     added_date : {
         type : Date,
         default : Date.now
-    }
+    },
+    pic:{
+        type:String,
+        default:"https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+       },
+    followers:[{type:ObjectId,ref:"User"}],
+    following:[{type:ObjectId,ref:"User"}]
 })
 
 // Export Schema
